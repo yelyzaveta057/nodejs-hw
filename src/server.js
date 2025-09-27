60938594a918eb614e9da78e492e9da75ffcfcc4
yelyzaveta057 <lisa.rodionova2002@gmail.com>
2025-09-26 20:56:21 -0400

    add

--- src/server.js
+++ src/server.js
@@ -1,9 +1,13 @@
 
 import express from 'express';
+import cors from 'cors';
 import 'dotenv/config';
 
 const app = express();
 const PORT = process.env.PORT ?? 3030;
+
+app.use(express.json());
+app.use(cors());
 
 
 app.get('/notes', (req, res) => {
