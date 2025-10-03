02ff942b55378fcdd086ea7eb2f7da6098aadb94
yelyzaveta057 <lisa.rodionova2002@gmail.com>
2025-10-02 22:16:01 -0400

    add

--- src/controllers/notesControllers.js
+++ src/controllers/notesControllers.js
@@ -9,10 +9,48 @@ export const getAllNotes = async (req, res) => {
   res.status(200).json(notes);
 };
 
+ export const createNote = async (req, res) => {
+  const note = await Note.create(req.body);
+  res.status(201).json(note);
+};
+
 // Отримати одної нотатки за id
 export const getNoteById = async (req, res, next) => {
   const { noteId } = req.params;
   const note = await Note.findById(noteId);
+
+
+  if (!note) {
+    next(createHttpError(404, 'Note not found'));
+    return;
+  }
+
+  res.status(200).json(note);
+};
+
+
+export const deleteNote = async (req, res, next) => {
+  const { notetId } = req.params;
+  const note = await Note.findOneAndDelete({
+    id: notetId,
+  });
+
+  if (!note) {
+    next(createHttpError(404, "Note not found"));
+    return;
+  }
+
+  res.status(200).send(note);
+};
+
+export const updateNote = async (req, res, next) => {
+  const { notetId } = req.params;
+
+  const note = await Note.findOneAndUpdate(
+    { _id: notetId }, // Шукаємо по id
+    req.body,
+    { new: true }, // повертаємо оновлений документ
+  );
 
   if (!note) {
     next(createHttpError(404, 'Note not found'));
--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,7 +1,7 @@
 // src/routes/studentsRoutes.js
 
 import { Router } from 'express';
-import { getAllNotes, getNoteById } from '../controllers/notesControllers.js';
+import { createNote, deleteNote, getAllNotes, getNoteById, updateNote } from '../controllers/notesControllers.js';
 
 
 
@@ -9,5 +9,8 @@ const router = Router();
 
 router.get("/notes", getAllNotes);
 router.get("/notes/:notetId", getNoteById);
+router.post('/notes', createNote);
+router.delete("/notes/:noteId", deleteNote);
+router.patch('/notes/:notetId', updateNote);
 
 export default router;
